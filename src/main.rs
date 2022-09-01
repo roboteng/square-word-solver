6de75a6529b04dc57574425ce2df74d0a871c2f9
Trevor Settles <roboteng@gmail.com>
2022-09-01 10:40:57 -0600

    filtered for all 5 letter words

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{fs::File, io::Read, path::Path};
+
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("/usr/share/dict/words");
+    let mut file = match File::open(path) {
+        Ok(file) => file,
+        Err(err) => panic!("Couldn't open {:?} because {}", path, err),
+    };
+
+    let reg = Regex::new("^[a-z]{5}$").unwrap();
+    let mut string = Box::new(String::new());
+    file.read_to_string(&mut string).unwrap();
+    let words: Vec<&str> = string.split('\n').collect();
+    let valid_words: Vec<&str> = words
+        .into_iter()
+        .filter(|word| -> bool { reg.is_match(word) })
+        .collect();
+
+    println!("{:?}", valid_words);
 }
