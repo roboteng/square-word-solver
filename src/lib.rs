05cc244b10192ecbe073bd95a1f390ec9d98b00a
Trevor Settles <roboteng@gmail.com>
2022-09-15 21:19:21 -0600

    found solutions, and added bench

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
+#![feature(test)]
 pub mod solver;
-
+extern crate test;
 use regex::Regex;
 use std::{fs::File, io::Read, path::Path};
 
 pub fn get_words() -> Vec<String> {
-    let path = Path::new("/usr/share/dict/words");
+    // let path = Path::new("/usr/share/dict/words");
+    let path = Path::new("sgb-words.txt");
     let mut file = match File::open(path) {
         Ok(file) => file,
         Err(err) => panic!("Couldn't open {:?} because {}", path, err),
@@ -24,7 +26,7 @@ pub fn five_letter_words(string: &str) -> Vec<String> {
         .collect()
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Solution<'a> {
     rows: Vec<&'a str>,
 }
@@ -32,6 +34,19 @@ pub struct Solution<'a> {
 impl<'a> Solution<'a> {
     pub fn new(rows: Vec<&'a str>) -> Solution {
         Solution { rows }
+    }
+
+    fn columns(&self) -> Vec<String> {
+        let mut columns: Vec<String> = vec![];
+        for i in 0..5 {
+            let s: String = self
+                .rows
+                .iter()
+                .map(|row| row.as_bytes()[i] as char)
+                .collect();
+            columns.push(s);
+        }
+        columns
     }
 }
 
@@ -58,15 +73,42 @@ pub fn find_solutions<'a>(
     _possible_columns: &WordList,
     _possible_rows: &'a Vec<&str>,
 ) -> Vec<Solution<'a>> {
-    if _possible_rows.len() == 0 {
-        return vec![];
+    _find_solutions(_possible_columns, _possible_rows, Solution::new(vec![]), 0)
+}
+
+fn _find_solutions<'a>(
+    possible_columns: &WordList,
+    possible_rows: &'a Vec<&str>,
+    in_progress_solution: Solution<'a>,
+    row_index: usize,
+) -> Vec<Solution<'a>> {
+    let mut solutions = vec![];
+    for word in possible_rows.iter() {
+        let mut sol = in_progress_solution.clone();
+        sol.rows.push(word);
+        if sol
+            .columns()
+            .iter()
+            .all(|column| possible_columns.contains(column))
+        {
+            if sol.rows.len() == 5 {
+                if sol.columns() != sol.rows {
+                    println!("Found: {:?}", sol);
+                    solutions.push(sol);
+                }
+            } else {
+                let mut sols = _find_solutions(possible_columns, possible_rows, sol, row_index + 1);
+                solutions.append(&mut sols);
+            }
+        }
     }
-    vec![Solution::new(_possible_rows.clone())]
+    solutions
 }
 
 #[cfg(test)]
 mod test2 {
     use super::*;
+    use test::Bencher;
 
     #[test]
     fn empty_word_list_does_not_contain_a_word() {
@@ -101,6 +143,13 @@ mod test2 {
     }
 
     #[test]
+    fn columns() {
+        let solution = Solution::new(vec!["grime", "honor"]);
+        let expected = vec!["gh", "ro", "in", "mo", "er"];
+        assert_eq!(solution.columns(), expected);
+    }
+
+    #[test]
     fn finds_1_solution_with_word_list_from_known_solution() {
         let rows = vec!["grime", "honor", "outdo", "steed", "terse"];
         let columns = vec!["ghost", "route", "inter", "modes", "erode"];
@@ -108,4 +157,20 @@ mod test2 {
         let solutions = find_solutions(&list, &rows);
         assert_eq!(solutions, vec![Solution::new(rows.clone())]);
     }
+
+    #[bench]
+    fn dict_test(b: &mut Bencher) {
+        let binding = get_words();
+        let words: Vec<&str> = binding.iter().map(|s| s.as_str()).collect();
+
+        let list = WordList::new(words.clone());
+        let first = words[0];
+        let last = words[words.len() - 1];
+        b.iter(|| {
+            list.contains(first);
+            list.contains(last);
+        })
+    }
 }
+
+// Found: Solution { rows: ["which", "hello", "olios", "place", "socks"] }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,16 @@ use square_word::*;
 
 fn main() {
     let valid_words = get_words();
+    let valid_words: Vec<&str> = valid_words.iter().map(|s| s.as_str()).collect();
 
     println!("{:?}", valid_words);
 
-    let _list = WordList::new(valid_words.iter().map(|w| w.as_str()).collect());
+    // let valid_words = vec![
+    //     "grime", "honor", "outdo", "steed", "terse", "ghost", "route", "inter", "modes", "erode",
+    //     "level", "oxide", "atria", "truck", "hasty", "loath", "extra", "virus", "edict", "leaky",
+    // ];
+
+    let list = WordList::new(valid_words.clone());
+    let solutions = find_solutions(&list, &valid_words);
+    println!("{:?}", solutions);
 }
