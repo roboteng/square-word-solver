605a2ab980542d678ccef0da0dbf2342641faf4b
Trevor Settles <roboteng@gmail.com>
2023-02-09 20:20:46 -0700

    reduced duplicate computation

--- src/solver.rs
+++ src/solver.rs
@@ -27,12 +27,15 @@ impl Puzzle {
     }
 
     pub fn view(&self) -> PuzzleViewModel {
+        let grid = self.grid();
+        let hints = self.hints(&grid);
+        let alphabet = self.alphabet(&hints);
         PuzzleViewModel {
             guesses: self.guesses(),
-            is_finished: self.is_finished(),
-            grid: self.grid(),
-            hints: self.hints(),
-            alphabet: self.alphabet(),
+            is_finished: self.is_finished(&grid),
+            grid,
+            hints,
+            alphabet,
         }
     }
 
@@ -40,8 +43,10 @@ impl Puzzle {
         self.guesses.clone()
     }
 
-    fn is_finished(&self) -> bool {
-        false
+    fn is_finished(&self, grid: &[[Option<AsciiChar>; 5]; 5]) -> bool {
+        grid.iter()
+            .map(|row| row.iter().all(|l| l.is_some()))
+            .all(|r| r)
     }
 
     fn grid(&self) -> [[Option<AsciiChar>; 5]; 5] {
@@ -55,12 +60,10 @@ impl Puzzle {
                 }
             }
         }
-
         arr
     }
 
-    fn hints(&self) -> [AsciiString; 5] {
-        let grid = self.grid();
+    fn hints(&self, grid: &[[Option<AsciiChar>; 5]; 5]) -> [AsciiString; 5] {
         let hints = self
             .solution
             .rows
@@ -71,7 +74,7 @@ impl Puzzle {
         hints.try_into().unwrap()
     }
 
-    fn alphabet(&self) -> BTreeMap<AsciiChar, LetterPlayed> {
+    fn alphabet(&self, hints: &[AsciiString]) -> BTreeMap<AsciiChar, LetterPlayed> {
         let mut dict = BTreeMap::new();
         let grid_letters = self
             .solution
@@ -80,8 +83,7 @@ impl Puzzle {
             .map(|word| word.chars())
             .flatten()
             .collect::<Vec<_>>();
-        let hint_letters = self
-            .hints()
+        let hint_letters = hints
             .iter()
             .map(|word| word.chars())
             .flatten()
@@ -197,6 +199,176 @@ mod test {
                 alphabet.insert(AsciiChar::o, LetterPlayed::PartiallyUsed);
                 alphabet.insert(AsciiChar::s, LetterPlayed::PartiallyUsed);
                 alphabet.insert(AsciiChar::e, LetterPlayed::PartiallyUsed);
+                alphabet
+            },
+        };
+
+        let actual = puzzle.view();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn after_guessing_four_times() {
+        let mut puzzle = Puzzle::new(Solution::new(vec![
+            "grime", "honor", "outdo", "steed", "terse",
+        ]));
+
+        puzzle.guess(AsciiString::from_ascii("grime").unwrap());
+        puzzle.guess(AsciiString::from_ascii("honor").unwrap());
+        puzzle.guess(AsciiString::from_ascii("outdo").unwrap());
+        puzzle.guess(AsciiString::from_ascii("steed").unwrap());
+
+        let expected = PuzzleViewModel {
+            guesses: vec![
+                AsciiString::from_ascii("grime").unwrap(),
+                AsciiString::from_ascii("honor").unwrap(),
+                AsciiString::from_ascii("outdo").unwrap(),
+                AsciiString::from_ascii("steed").unwrap(),
+            ],
+            is_finished: false,
+            grid: [
+                [
+                    Some(AsciiChar::g),
+                    Some(AsciiChar::r),
+                    Some(AsciiChar::i),
+                    Some(AsciiChar::m),
+                    Some(AsciiChar::e),
+                ],
+                [
+                    Some(AsciiChar::h),
+                    Some(AsciiChar::o),
+                    Some(AsciiChar::n),
+                    Some(AsciiChar::o),
+                    Some(AsciiChar::r),
+                ],
+                [
+                    Some(AsciiChar::o),
+                    Some(AsciiChar::u),
+                    Some(AsciiChar::t),
+                    Some(AsciiChar::d),
+                    Some(AsciiChar::o),
+                ],
+                [
+                    Some(AsciiChar::s),
+                    Some(AsciiChar::t),
+                    Some(AsciiChar::e),
+                    Some(AsciiChar::e),
+                    Some(AsciiChar::d),
+                ],
+                [None, None, None, None, Some(AsciiChar::e)],
+            ],
+            hints: [
+                AsciiString::from_ascii("").unwrap(),
+                AsciiString::from_ascii("").unwrap(),
+                AsciiString::from_ascii("").unwrap(),
+                AsciiString::from_ascii("").unwrap(),
+                AsciiString::from_ascii("rets").unwrap(),
+            ],
+            alphabet: {
+                let mut alphabet = BTreeMap::new();
+                // "grime", "honor", "outdo", "steed", "terse"
+                alphabet.insert(AsciiChar::g, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::r, LetterPlayed::PartiallyUsed);
+                alphabet.insert(AsciiChar::i, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::m, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::e, LetterPlayed::PartiallyUsed);
+                alphabet.insert(AsciiChar::h, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::o, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::n, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::u, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::d, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::s, LetterPlayed::PartiallyUsed);
+                alphabet.insert(AsciiChar::t, LetterPlayed::PartiallyUsed);
+                alphabet
+            },
+        };
+
+        let actual = puzzle.view();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn full_solution() {
+        let mut puzzle = Puzzle::new(Solution::new(vec![
+            "grime", "honor", "outdo", "steed", "terse",
+        ]));
+
+        puzzle.guess(AsciiString::from_ascii("grime").unwrap());
+        puzzle.guess(AsciiString::from_ascii("honor").unwrap());
+        puzzle.guess(AsciiString::from_ascii("outdo").unwrap());
+        puzzle.guess(AsciiString::from_ascii("steed").unwrap());
+        puzzle.guess(AsciiString::from_ascii("terse").unwrap());
+
+        let expected = PuzzleViewModel {
+            guesses: vec![
+                AsciiString::from_ascii("grime").unwrap(),
+                AsciiString::from_ascii("honor").unwrap(),
+                AsciiString::from_ascii("outdo").unwrap(),
+                AsciiString::from_ascii("steed").unwrap(),
+                AsciiString::from_ascii("terse").unwrap(),
+            ],
+            is_finished: true,
+            grid: [
+                [
+                    Some(AsciiChar::g),
+                    Some(AsciiChar::r),
+                    Some(AsciiChar::i),
+                    Some(AsciiChar::m),
+                    Some(AsciiChar::e),
+                ],
+                [
+                    Some(AsciiChar::h),
+                    Some(AsciiChar::o),
+                    Some(AsciiChar::n),
+                    Some(AsciiChar::o),
+                    Some(AsciiChar::r),
+                ],
+                [
+                    Some(AsciiChar::o),
+                    Some(AsciiChar::u),
+                    Some(AsciiChar::t),
+                    Some(AsciiChar::d),
+                    Some(AsciiChar::o),
+                ],
+                [
+                    Some(AsciiChar::s),
+                    Some(AsciiChar::t),
+                    Some(AsciiChar::e),
+                    Some(AsciiChar::e),
+                    Some(AsciiChar::d),
+                ],
+                [
+                    Some(AsciiChar::t),
+                    Some(AsciiChar::e),
+                    Some(AsciiChar::r),
+                    Some(AsciiChar::s),
+                    Some(AsciiChar::e),
+                ],
+            ],
+            hints: [
+                AsciiString::from_ascii("").unwrap(),
+                AsciiString::from_ascii("").unwrap(),
+                AsciiString::from_ascii("").unwrap(),
+                AsciiString::from_ascii("").unwrap(),
+                AsciiString::from_ascii("").unwrap(),
+            ],
+            alphabet: {
+                let mut alphabet = BTreeMap::new();
+                // "grime", "honor", "outdo", "steed", "terse"
+                alphabet.insert(AsciiChar::g, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::r, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::i, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::m, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::e, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::h, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::o, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::n, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::u, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::d, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::s, LetterPlayed::AllUsed);
+                alphabet.insert(AsciiChar::t, LetterPlayed::AllUsed);
                 alphabet
             },
         };
