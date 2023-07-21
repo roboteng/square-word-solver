84bc0678ddbb0aef2a5b2eb5532a41e146ed6053
Trevor Settles <roboteng@gmail.com>
2023-07-20 20:39:20 -0600

    feat: initial implementation of information

--- src/first_guess.rs
+++ src/first_guess.rs
@@ -1,5 +1,12 @@
-pub fn info(_distrobution: &[u32]) -> f64 {
-    1.0
+pub fn info(distrobution: &[u32]) -> f64 {
+    let size = distrobution.iter().sum::<u32>() as f64;
+    distrobution
+        .iter()
+        .map(|&a| {
+            let k: f64 = (a as f64 / size);
+            -k.log2() / size
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -8,10 +15,18 @@ mod test {
 
     #[test]
     fn gives_correct_for_distrobution() {
-        let distrobution = [1];
-        let actual = info(&distrobution);
-        let expected = 1.0;
+        fn check(dist: &[u32], expected: f64) {
+            let actual = info(&dist);
 
-        assert_eq!(actual, expected);
+            assert_eq!(actual, expected, "For distrobution: {dist:?} expected an information of {expected}, but got {actual}");
+        }
+
+        for (distrobution, expected) in [
+            //split
+            (vec![1], 0.0),
+            (vec![1, 1], 1.0),
+        ] {
+            check(&distrobution, expected)
+        }
     }
 }
